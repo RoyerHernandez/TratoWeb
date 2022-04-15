c11ae93722482bae3bcdc8598a9e9c908b9cc2b8
Royer Alfonso Hernandez Morales <royer.hernandez@servientrega.com>
2022-04-15 08:59:02 -0500

    Finish component Exchanges

--- TratoWeb/src/app/exchanges/exchanges.component.ts
+++ TratoWeb/src/app/exchanges/exchanges.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { exchanges } from '../exchanges/exchanges.model'
 
 @Component({
   selector: 'app-exchanges',
@@ -8,6 +9,64 @@ import { Component, OnInit } from '@angular/core';
 export class ExchangesComponent implements OnInit {
 
   constructor() { }
+
+  exchange: exchanges[]=
+  [
+    {
+      id: '1',
+      productName: 'camiseta',
+      exchangesValue: '100',
+      image: 'assets/images/hoodie.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    },
+    {
+      id: '2',
+      productName: 'stickers1',
+      exchangesValue: '100',
+      image: 'assets/images/stickers1.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    },
+    {
+      id: '3',
+      productName: 'stickers2',
+      exchangesValue: '100',
+      image: 'assets/images/stickers2.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    },
+    {
+      id: '4',
+      productName: 'pin',
+      exchangesValue: '100',
+      image: 'assets/images/pin.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    },
+    {
+      id: '5',
+      productName: 'mug',
+      exchangesValue: '100',
+      image: 'assets/images/mug.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    },
+    {
+      id: '6',
+      productName: 'camiseta',
+      exchangesValue: '100',
+      image: 'assets/images/camiseta.png',
+      startValue: '05:00',
+      exchangesState: 'assets/images/logo/union.png',
+      description: 'Espacio para foto'
+    }
+  ]
 
   ngOnInit(): void {
   }
